8289eab64bf9aabfb5af46cbc1f0c67ae3baa7eb
Yingli Lu <yinglilu@gmail.com>
2023-11-03 19:30:50 -0400

    add test

--- tests/test_command_lines.py
+++ tests/test_command_lines.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 
 def test_command_line_less_than_3_args():
@@ -23,3 +24,15 @@ def test_command_line_input_dcmfile_output_path_to_dcmfile():
     command = "python dcm_anon.py tests/CT/CT_small.dcm tests/temp/test.dcm"
     r = os.system(command)
     assert r == 0
+
+
+def test_command_line_input_folder_output_folder():
+    command = "python dcm_anon.py tests/CT tests/temp"
+    r = os.system(command)
+    assert r == 0
+
+
+def test_remove_temp_dir():
+    if os.path.exists("tests/temp"):
+        shutil.rmtree("tests/temp")
+        assert 0 == 0
