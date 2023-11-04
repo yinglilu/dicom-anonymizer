754f2b6955fe9afde7241b09fd15ca4f70cd1d34
Yingli Lu <yinglilu@gmail.com>
2023-11-03 21:08:54 -0400

    modified test_command_lines.py

--- tests/test_command_lines.py
+++ tests/test_command_lines.py
@@ -5,19 +5,19 @@ import shutil
 def test_command_line_less_than_3_args():
     command = "python dcm_anon.py"
     r = os.system(command)
-    assert r == 1
+    assert r != 0
 
 
 def test_command_line_input_not_exist():
     command = "python dcm_anon.py non-exist-file.dcm output.dcm"
     r = os.system(command)
-    assert r == 1
+    assert r != 0
 
 
 def test_command_line_input_file_but_output_folder():
     command = "python dcm_anon.py tests/CT/CT_small.dcm tests"
     r = os.system(command)
-    assert r == 1
+    assert r != 0
 
 
 def test_command_line_input_dcmfile_output_path_to_dcmfile():
